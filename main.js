db3ebcea1a1f043b68bce3cd07da214377414063
zanebliss <zanebliss@icloud.com>
2020-05-04 08:29:23 -0500

    additional changes

--- main.js
+++ main.js
@@ -1,25 +1,62 @@
-headerEls = document.querySelector(".header-container").childNodes;
 
-headerEls.forEach(element => {
-    $(element).css({opacity: "0"})
+
+headerEls = document.querySelector(".header-container").childNodes;
+content_1Els = document.querySelector(".content_1").childNodes;
+
+content_1Els.forEach(element => {
+  $(element).css({ opacity: "0" });
 });
 
-for (let i=0; i<10; i++) { 
-    fade(i); 
-    
- } 
- function fade(i) { 
-   setTimeout(function() { 
-       if (headerEls[i].className == "line") {
-           $(headerEls[i]).fadeTo(3000, 1).animate({
-               width: "50%"
-           }, 2000)
-       } else if (headerEls[i].id == "heading") {
-           $(headerEls[i]).fadeTo(3000, 1).animate({
-               letterSpacing: "10px"
-           }, 4000)
-       } else {
-        $(headerEls[i]).fadeTo(3000, 1)
-       }
-   }, 500 * i); 
- } 
+headerEls.forEach((element) => {
+  $(element).css({ opacity: "0" });
+});
+
+function startFade() {
+  for (let i = 0; i < 10; i++) {
+    fade(i, headerEls)
+    fade(i, content_1Els);
+  }
+}
+function fade(i, container) {
+  setTimeout(function () {
+    if (container[i].className == "line") {
+      $(container[i]).fadeTo(3000, 1).animate(
+        {
+          width: "120%",
+        },
+        3000
+      );
+    } else if (container[i].className == "heading") {
+      $(container[i]).fadeTo(3000, 1).animate(
+        {
+          letterSpacing: "10px",
+        },
+        4000
+      );
+    } else {
+      $(container[i]).fadeTo(3000, 1);
+    }
+  }, 500 * i);
+}
+
+
+function animatethis(targetElement, speed) {
+  $(targetElement).animate({ "background-position-x": "80%" },
+  {
+      duration: speed,
+      complete: function ()
+      {
+          targetElement.animate({ "background-position-x": "0%" },
+          {
+              duration: speed,
+              complete: function ()
+              {
+                  animatethis(targetElement, speed);
+              }
+          });
+      }
+  });
+};
+animatethis($('.parent-container_2'), 15000);
+
+startFade()
